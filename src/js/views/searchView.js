21ed6e0bd60c296268a5c1eab06a63e7fa64471d
kshitijvarshney5 <kshitijvarshney51@outlook.com>
2024-04-22 22:19:36 +0530

    first commit

---
+++ src/js/views/searchView.js
@@ -0,0 +1,24 @@
+import { state } from "../model.js";
+class SearchView {
+  _parentEl = document.querySelector(".search");
+
+  getQuery() {
+    const query = this._parentEl.querySelector(".search__field").value;
+    console.log(query);
+    state.search.page = 1;
+    this._clearView();
+    return query;
+  }
+  _clearView() {
+    this._parentEl.querySelector(".search__field").value = "";
+  }
+
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener("submit", function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
